f7ef837bcf1c0c9997218867ae172b742c6b7b9d
Luiz H. Celeguim <luiz.celeguim@gmail.com>
2022-05-27 09:57:38 +0200

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+// import i18n from "boot/i18n";
+import { i18n } from "boot/i18n";
+
+console.log(i18n.global.t("success"));
+console.log(i18n);
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
